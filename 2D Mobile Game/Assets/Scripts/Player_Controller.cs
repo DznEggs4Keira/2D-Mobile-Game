2a4dc65c78a8a46651c97405c79e3dfaa96136d4
Zainab Ali <DznEggs4Keira@users.noreply.github.com>
2020-12-07 12:31:20 +0400

    Scrolling BAckground Works. Testing Touch Controls

---
+++ 2D Mobile Game/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    Touch_Controls touch;
+
+    //When dealing with physics, it's better to use Fixed Updates
+    private void FixedUpdate()
+    {
+        switch(Touch_Controls.swipeDirection)
+        {
+            case Touch_Controls.Swipe.None:
+            default:
+                break;
+
+            //Dashing
+            case Touch_Controls.Swipe.Up:
+                Debug.Log("Dash Forwards");
+                break;
+
+            case Touch_Controls.Swipe.Down:
+                Debug.Log("Dash Backwards");
+                break;
+
+            //Shifting
+            case Touch_Controls.Swipe.Left:
+                Debug.Log("Shift to the Left");
+                break;
+
+            case Touch_Controls.Swipe.Right:
+                Debug.Log("Shift to the Right");
+                break;
+        }
+    }
+}
---
+++ 2D Mobile Game/Assets/Scripts/Touch_Controls.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch_Controls : MonoBehaviour
+{
+public enum Swipe { None, Up, Down, Left, Right };
+
+    public float minSwipeLength = 200f;
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+
+    public static Swipe swipeDirection;
+
+    void Update()
+    {
+        DetectSwipe();
+    }
+
+    public void DetectSwipe()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+
+            if (t.phase == TouchPhase.Ended)
+            {
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                // Make sure it was a legit swipe, not a tap
+                if (currentSwipe.magnitude < minSwipeLength)
+                {
+                    swipeDirection = Swipe.None;
+                    return;
+                }
+
+                currentSwipe.Normalize();
+
+                // Swipe up
+                if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                    swipeDirection = Swipe.Up;
+                
+                // Swipe down
+                else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                    swipeDirection = Swipe.Down;
+
+                // Swipe left
+                else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                    swipeDirection = Swipe.Left;
+
+                // Swipe right
+                else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                    swipeDirection = Swipe.Right;
+            }
+        }
+        else
+        {
+            swipeDirection = Swipe.None;
+        }
+    }
+}
